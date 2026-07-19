5bf03e639a4c54df3e35aeb13d989d1748622003
sahara256 <ssahara284@gmail.com>
2026-07-19 06:08:04 +0530

    clean up duplicate accuracy_score imports

--- src/train_model.py
+++ src/train_model.py
@@ -1,7 +1,6 @@
 import joblib
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 from sklearn.metrics import (
     accuracy_score,
     classification_report
@@ -30,8 +29,6 @@ model = LogisticRegression(
 # ---------------------------------
 model.fit(X_train, y_train)
 train_pred = model.predict(X_train)
-
-from sklearn.metrics import accuracy_score
 
 print("Training Accuracy :", accuracy_score(y_train, train_pred))
 
